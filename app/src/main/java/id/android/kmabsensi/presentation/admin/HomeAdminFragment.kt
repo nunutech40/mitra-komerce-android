30cfe638c9ac466661cc476ac550726da6688753
riefist <muhamadarief3101@gmail.com>
2019-09-16 22:31:44 +0800

    admin home and report ui

---
+++ app/src/main/java/id/android/kmabsensi/presentation/admin/HomeAdminFragment.kt
@@ -0,0 +1,26 @@
+package id.android.kmabsensi.presentation.admin
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import id.android.kmabsensi.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class HomeAdminFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_admin, container, false)
+    }
+
+
+}
