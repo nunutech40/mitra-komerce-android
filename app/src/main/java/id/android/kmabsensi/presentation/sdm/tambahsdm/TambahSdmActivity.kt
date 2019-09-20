bbf70ae6a20832550f7da5a004a5f14a17097196
riefist <muhamadarief3101@gmail.com>
2019-09-21 07:25:43 +0800

    tambah sdm ui (progress)

---
+++ app/src/main/java/id/android/kmabsensi/presentation/sdm/tambahsdm/TambahSdmActivity.kt
@@ -0,0 +1,13 @@
+package id.android.kmabsensi.presentation.sdm.tambahsdm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.android.kmabsensi.R
+
+class TambahSdmActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tambah_sdm)
+    }
+}
