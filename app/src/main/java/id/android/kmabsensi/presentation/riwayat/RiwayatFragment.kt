65ecf03680983826ba787002506463209c141400
riefist <muhamadarief3101@gmail.com>
2019-10-03 21:58:39 +0800

    riwayat presence management

--- app/src/main/java/id/android/kmabsensi/presentation/riwayat/RiwayatFragment.kt
+++ app/src/main/java/id/android/kmabsensi/presentation/riwayat/RiwayatFragment.kt
@@ -6,14 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.ViewHolder
 
 import id.android.kmabsensi.R
+import id.android.kmabsensi.data.remote.response.User
+import id.android.kmabsensi.presentation.home.HomeViewModel
+import id.android.kmabsensi.utils.UiState
+import id.android.kmabsensi.utils.gone
+import id.android.kmabsensi.utils.visible
 import kotlinx.android.synthetic.main.fragment_riwayat.*
 import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 /**
  * A simple [Fragment] subclass.
@@ -21,7 +28,34 @@ import org.koin.android.ext.android.inject
 class RiwayatFragment : Fragment() {
 
     private val vm: RiwayatViewModel by inject()
+    private val homeViewModel: HomeViewModel by sharedViewModel()
     private val groupAdapter = GroupAdapter<ViewHolder>()
+
+    lateinit var user: User
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        vm.riwayatResponse.observe(viewLifecycleOwner, Observer {
+            when (it) {
+                is UiState.Loading -> {
+                    progressBar.visible()
+                }
+                is UiState.Success -> {
+                    progressBar.gone()
+                    it.data.data.forEach {
+                        groupAdapter.add(RiwayatItem(it, user.full_name, user.photo_profile_url))
+                    }
+                }
+                is UiState.Error -> {
+                    progressBar.gone()
+                }
+            }
+        })
+
+        user = homeViewModel.getUserData()
+        vm.getPresenceHistory(user.id)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,7 +72,7 @@ class RiwayatFragment : Fragment() {
 
     }
 
-    fun initRv(){
+    fun initRv() {
         val linearLayoutManager = LinearLayoutManager(context)
         rvRiwayat.apply {
             layoutManager = linearLayoutManager
--- app/src/main/java/id/android/kmabsensi/presentation/riwayat/RiwayatItem.kt
+++ app/src/main/java/id/android/kmabsensi/presentation/riwayat/RiwayatItem.kt
@@ -5,17 +5,23 @@ import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 import id.android.kmabsensi.data.remote.response.PresenceHistory
 import id.android.kmabsensi.R
 import id.android.kmabsensi.data.pref.PreferencesHelper
+import id.android.kmabsensi.utils.loadCircleImage
+import kotlinx.android.synthetic.main.item_row_absensi.*
 import kotlinx.android.synthetic.main.item_row_absensi.view.*
 
 class RiwayatItem(val presenceHistory: PresenceHistory,
                   val userName: String,
-                  val userPict: String) : Item() {
+                  val userPict: String?) : Item() {
 
     override fun bind(viewHolder: ViewHolder, position: Int) {
 
         viewHolder.apply {
             itemView.txtNama.text = userName
-//            itemView.txtCheckIn.text =
+            itemView.txtCheckIn.text = presenceHistory.check_in_datetime.split(" ")[1]
+            itemView.txtCheckOut.text = presenceHistory.checkout_date_time.split(" ")[1]
+
+            imgProfile.loadCircleImage(userPict.toString())
+
         }
 
     }
