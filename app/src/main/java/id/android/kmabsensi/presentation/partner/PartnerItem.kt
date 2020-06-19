8d9b56221ba2251e0b76fc0516fc728a85a1642a
riefist <muhamadarief3101@gmail.com>
2020-06-19 14:46:58 +0800

    capitalize each word in partner name

--- app/src/main/java/id/android/kmabsensi/presentation/partner/PartnerItem.kt
+++ app/src/main/java/id/android/kmabsensi/presentation/partner/PartnerItem.kt
@@ -4,6 +4,7 @@ import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
 import id.android.kmabsensi.R
 import id.android.kmabsensi.data.remote.response.Partner
+import id.android.kmabsensi.utils.capitalizeWords
 import kotlinx.android.synthetic.main.item_row_partner.view.*
 
 class PartnerItem(val partner: Partner,
@@ -12,7 +13,7 @@ class PartnerItem(val partner: Partner,
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
         viewHolder.apply {
             itemView.textPartnerNumber.text = "NO. PARTNER ${partner.noPartner}"
-            itemView.textPartnerName.text = partner.fullName
+            itemView.textPartnerName.text = partner.fullName.capitalizeWords()
             itemView.textPartnerCategory.text = partner.partnerDetail.partnerCategoryName
             itemView.textTotalSdm.text = partner.totalSdmAssigned.toString()
 
