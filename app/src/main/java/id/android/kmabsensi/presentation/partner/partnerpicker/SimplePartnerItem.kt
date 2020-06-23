0fd4c2c17348186a287d6aec4390ae29bb00cce6
riefist <muhamadarief3101@gmail.com>
2020-06-23 17:24:01 +0800

    capitalize full name partner

--- app/src/main/java/id/android/kmabsensi/presentation/partner/partnerpicker/SimplePartnerItem.kt
+++ app/src/main/java/id/android/kmabsensi/presentation/partner/partnerpicker/SimplePartnerItem.kt
@@ -4,13 +4,14 @@ import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
 import id.android.kmabsensi.R
 import id.android.kmabsensi.data.remote.response.SimplePartner
+import id.android.kmabsensi.utils.capitalizeWords
 import kotlinx.android.synthetic.main.item_row_simple_partners.view.*
 
 class SimplePartnerItem(val simplePartner: SimplePartner,
                         val listener: (SimplePartner) -> Unit): Item(){
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
         viewHolder.apply {
-            itemView.textPartner.text = "${simplePartner.noPartner} - ${simplePartner.fullName}"
+            itemView.textPartner.text = "${simplePartner.noPartner} - ${simplePartner.fullName.capitalizeWords()}"
 
             itemView.setOnClickListener {
                 listener(simplePartner)
