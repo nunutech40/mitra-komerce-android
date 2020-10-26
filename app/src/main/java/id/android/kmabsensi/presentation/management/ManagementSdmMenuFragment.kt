beaad10d65bff19fdb91a3502ba35216e5c32245
KM Studio <kmstudio.id@gmail.com>
2020-10-26 09:51:17 +0700

    add Hari Libur  menu to Role SDM

--- app/src/main/java/id/android/kmabsensi/presentation/management/ManagementSdmMenuFragment.kt
+++ app/src/main/java/id/android/kmabsensi/presentation/management/ManagementSdmMenuFragment.kt
@@ -14,6 +14,7 @@ import id.android.kmabsensi.presentation.permission.manajemenizin.ManajemenIzinA
 import id.android.kmabsensi.presentation.sdm.KelolaDataSdmActivity
 import id.android.kmabsensi.presentation.sdm.dataalfa.DataAlfaActivity
 import id.android.kmabsensi.presentation.sdm.device.DataDeviceActivity
+import id.android.kmabsensi.presentation.sdm.holiday.HolidayActivity
 import id.android.kmabsensi.presentation.sdm.modekerja.ModeKerjaActivity
 import id.android.kmabsensi.presentation.sdm.nonjob.SdmNonJobActivity
 import id.android.kmabsensi.utils.IS_MANAGEMENT_KEY
@@ -85,6 +86,10 @@ class ManagementSdmMenuFragment : Fragment() {
 
         btnDataDevice.setOnClickListener {
             activity?.startActivity<DataDeviceActivity>()
+        }
+
+        btnHariLibur.setOnClickListener {
+            activity?.startActivity<HolidayActivity>()
         }
 
         btnDataAlfa.setOnClickListener {
