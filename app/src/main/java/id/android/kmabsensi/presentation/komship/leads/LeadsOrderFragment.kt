8838d81da12e6d76d3c01372bb1dfff01b4ea88b
Tamrizki <rizkitam77@gmail.com>
2021-09-21 06:37:40 +0700

    filter leads progress

--- app/src/main/java/id/android/kmabsensi/presentation/komship/leads/LeadsOrderFragment.kt
+++ app/src/main/java/id/android/kmabsensi/presentation/komship/leads/LeadsOrderFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import id.android.kmabsensi.R
 import id.android.kmabsensi.databinding.FragmentLeadsOrderBinding
 import id.android.kmabsensi.presentation.base.BaseFragmentRf
@@ -14,5 +15,15 @@ class LeadsOrderFragment : BaseFragmentRf<FragmentLeadsOrderBinding>(
 ) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding?.tiePartner?.setOnClickListener {
+            setupBottomSheat()
+        }
+    }
+
+    fun setupBottomSheat(){
+        val btnSheet = layoutInflater.inflate(R.layout.custom_toolbar_v2, null)
+        val dialog = BottomSheetDialog(requireContext())
+        dialog.setContentView(btnSheet)
+        dialog.show()
     }
 }
